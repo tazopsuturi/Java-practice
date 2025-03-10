f45b341f3137d51eea7b22641f757b74a6e69e26
TazoPsuturi <tazopsuturi33@gmail.com>
2025-03-10 12:00:00 +0400

    matrix

---
+++ transform_matrix-exercise/src/main/java/com/epam/rd/autotasks/matrix/MatrixUtil.java
@@ -0,0 +1,80 @@
+package com.epam.rd.autotasks.matrix;
+
+import java.util.Arrays;
+
+public class MatrixUtil {
+	public static void transformMatrix(int[][] matrix) {
+		if (matrix == null || matrix.length == 0) {
+			return;
+		}
+		
+		for (int[] row : matrix) {
+			if (row.length != matrix.length) {
+				return;
+			}
+		}
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (j < i) {
+					matrix[i][j] = 0;
+				} else if (j > i) {
+					matrix[i][j] = 1;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		{
+			int[][] matrix = null;
+			transformMatrix(matrix);
+			System.out.println(Arrays.deepToString(matrix));
+		}
+		{
+			int[][] matrix = {};
+			transformMatrix(matrix);
+			System.out.println(Arrays.deepToString(matrix));
+		}
+		{
+			int[][] matrix = { {} };
+			transformMatrix(matrix);
+			System.out.println(Arrays.deepToString(matrix));
+		}
+		{
+			int[][] matrix = { {}, {} };
+			transformMatrix(matrix);
+			System.out.println(Arrays.deepToString(matrix));
+		}
+		{
+			int[][] matrix = { { 2, 3 }, { 4, 5, 6 } };
+			transformMatrix(matrix);
+			System.out.println(Arrays.deepToString(matrix));
+		}
+		{
+			int[][] matrix = { { 2, 3 }, { 4, 5 }, {} };
+			transformMatrix(matrix);
+			System.out.println(Arrays.deepToString(matrix));
+		}
+		{
+			int[][] matrix = { { 2, 3 }, { 4, 5 } };
+			transformMatrix(matrix);
+			System.out.println(Arrays.deepToString(matrix));
+		}
+		{
+			int[][] matrix = { { 2, 4, 3, 3 }, { 5, 7, 8, 5 }, { 2, 4, 3, 3 }, { 5, 7, 8, 5 } };
+			transformMatrix(matrix);
+			System.out.println(Arrays.deepToString(matrix));
+		}
+		{
+			int[][] matrix = { { 2, 3 }, { 4, 5 }, { 6, 7 } };
+			transformMatrix(matrix);
+			System.out.println(Arrays.deepToString(matrix));
+		}
+		{
+			int[][] matrix = { { 2, 4, 3, 3 }, { 5, 7, 8, 5 }, { 2, 4, 3, 3 } };
+			transformMatrix(matrix);
+			System.out.println(Arrays.deepToString(matrix));
+		}
+	}
+}
